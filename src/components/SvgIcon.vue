6562b609f5ed1b8d126b1b5c266fccc40c96c10c
hoywu <wu_git@outlook.com>
2024-04-17 23:27:13 +0800

    feat: add SvgIcon component

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const ErrorIcon = () => import('@/assets/icon/code.vue');
+
+const props = defineProps<{
+  name: string;
+}>();
+const icon = defineAsyncComponent({
+  loader: () => import(`@/assets/icon/${props.name}.vue`).catch(ErrorIcon),
+});
+</script>
+
+<template>
+  <component :is="icon" />
+</template>
